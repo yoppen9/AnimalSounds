efb3d56a73f1cea1af95a6f95654266f3b0aa7d4
yoppen <yopppen.4403@gmail.com>
2021-05-12 15:14:34 +0900

    Initial Commit

---
+++ Shared/AnimalSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalSoundsApp.swift
+//  Shared
+//
+//  Created by 山岸善将 on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct AnimalSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 山岸善将 on 2021/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
